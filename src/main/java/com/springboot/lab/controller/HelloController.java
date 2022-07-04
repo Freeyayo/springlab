f5414c1074e450aeea2604c7097742c8e759ffc2
Kevin Cai <kevin_cai@epam.com>
2022-07-04 13:55:21 +0800

    feat: first api ;)

---
+++ src/main/java/com/springboot/lab/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.springboot.lab.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@RequestMapping("/hello")
+	public String hello() {
+		return "hello";
+	}
+	
+}
