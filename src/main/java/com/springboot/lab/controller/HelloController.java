f15a794d0d0329d815d96c3fc5705264302169ce
Kevin Cai <kevin_cai@epam.com>
2022-07-04 18:03:20 +0800

    feat: switch port & api

--- src/main/java/com/springboot/lab/controller/HelloController.java
+++ src/main/java/com/springboot/lab/controller/HelloController.java
@@ -1,14 +1,29 @@
 package com.springboot.lab.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class HelloController {
+//@RestController
+//public class HelloController {
+//
+//	@RequestMapping("/hello")
+//	public String hello() {
+//		return "hello";
+//	}
+//	
+//}
 
-	@RequestMapping("/hello")
-	public String hello() {
-		return "hello";
-	}
+@Controller
+@RequestMapping("/hello")
+public class HelloController {
 	
+	@GetMapping("/world")
+	@ResponseBody
+	public String hello() {
+		return "Hello World";
+	}
 }
+
